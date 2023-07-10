1143790ec3c3f977e0f2c5e56468900327c5791e
Miguel Xavier Penha Neto <miguel.neto@automattic.com>
2023-07-10 01:04:03 -0300

    Five

--- check-wpscan.php
+++ check-wpscan.php
@@ -1,129 +1,133 @@
 <?php
 
-function getPluginsAndThemes($wpContentPath) {
+function get_plugins_and_themes( $wpContentPath ) {
     $pluginsPath = $wpContentPath . '/plugins';
     $themesPath = $wpContentPath . '/themes';
-    
-    $plugins = [];
-    $themes = [];
-    
-    // Get plugin directories
-    if (is_dir($pluginsPath)) {
-        $pluginDirs = scandir($pluginsPath);
-        
-        foreach ($pluginDirs as $dir) {
-            if ($dir !== '.' && $dir !== '..' && is_dir($pluginsPath . '/' . $dir)) {
-                $plugin = [
-                    'name' => $dir,
-                    'version' => getPluginVersion($pluginsPath . '/' . $dir)
-                ];
-                
-                $plugins[] = $plugin;
-            }
-        }
-    }
-    
-    // Get theme directories
-    if (is_dir($themesPath)) {
-        $themeDirs = scandir($themesPath);
-        
-        foreach ($themeDirs as $dir) {
-            if ($dir !== '.' && $dir !== '..' && is_dir($themesPath . '/' . $dir)) {
-                $theme = [
-                    'name' => $dir,
-                    'version' => getThemeVersion($themesPath . '/' . $dir)
-                ];
-                
-                $themes[] = $theme;
-            }
-        }
-    }
-    
-    return [
-        'plugins' => $plugins,
-        'themes' => $themes
-    ];
+
+    $plugins = get_extension_with_version( $pluginsPath, 'plugin' );
+    $themes = get_extension_with_version( $themesPath, 'theme' );
+
+    return array_merge($plugins, $themes);
 }
 
-function getPluginVersion($pluginPath) {
-    $pluginFile = $pluginPath . '/' . basename($pluginPath) . '.php';
-    
-    if (file_exists($pluginFile)) {
-        $pluginData = getPluginData($pluginFile);
-        
+function get_extension_with_version( $path, $type ) {
+    $items = [];
+
+    if ( is_dir( $path ) ) {
+        $itemDirs = scandir( $path );
+
+        foreach ( $itemDirs as $dir ) {
+            $full_path = $path . '/' . $dir;
+
+            if ( $dir !== '.' && $dir !== '..' && is_dir( $full_path ) ) {
+                $item = [
+                    'name' => $dir,
+                    'version' => $type === 'plugin' ? get_plugin_data( $full_path ) : get_theme_data( $full_path ),
+                    'type' => $type,
+                    'slug' => basename($full_path)
+                ];
+
+                $items[] = $item;
+            }
+        }
+    }
+
+    return $items;
+}
+
+function get_plugin_data( $pluginPath ) {
+    $pluginFile = $pluginPath . '/' . basename( $pluginPath ) . '.php';
+
+    if ( file_exists( $pluginFile ) ) {
+        $pluginData = get_header_data( $pluginFile, 'plugin' );
+
         return $pluginData['Version'];
     }
-    
+
     return 'Unknown';
 }
 
-function getThemeVersion($themePath) {
+function get_theme_data( $themePath ) {
     $themeFile = $themePath . '/style.css';
-    
-    if (file_exists($themeFile)) {
-        $themeData = getThemeData($themeFile);
-        
+
+    if ( file_exists( $themeFile ) ) {
+        $themeData = get_header_data( $themeFile, 'theme' );
+
         return $themeData['Version'];
     }
-    
+
     return 'Unknown';
 }
 
-function getPluginData($pluginFile) {
-    $defaultHeaders = [
-        'Name' => 'Plugin Name',
-        'Version' => 'Version',
-        'Description' => 'Description',
-        'Author' => 'Author',
-        'AuthorURI' => 'Author URI',
-        'PluginURI' => 'Plugin URI'
-    ];
-    
-    $pluginData = getHeaderData($pluginFile, $defaultHeaders);
-    
-    return $pluginData;
-}
+function get_header_data( $file, $type ) {
+    $fileData = file_get_contents( $file );
 
-function getThemeData($themeFile) {
-    $defaultHeaders = [
-        'Theme Name' => 'Theme Name',
-        'Theme URI' => 'Theme URI',
-        'Description' => 'Description',
-        'Author' => 'Author',
-        'Author URI' => 'Author URI',
-        'Version' => 'Version'
-    ];
-    
-    $themeData = getHeaderData($themeFile, $defaultHeaders);
-    
-    return $themeData;
-}
-
-function getHeaderData($file, $defaultHeaders) {
-    $fileData = file_get_contents($file);
-    
     $headers = [];
-    
-    foreach ($defaultHeaders as $headerKey => $headerName) {
-        $regex = '/^' . $headerName . ':\s*(.+)/m';
-        
-        if (preg_match($regex, $fileData, $matches)) {
-            $headers[$headerKey] = $matches[1];
-        } else {
-            $headers[$headerKey] = 'Unknown';
-        }
+
+    $regex = $type === 'plugin' ? '/^\s*\*\s*Version:\s*(.*)$/m' : '/^Version:\s*(.*)$/m' ;
+
+    if (preg_match( $regex, $fileData, $matches ) ) {
+        $headers['Version'] = $matches[1];
+    } else {
+        $headers['Version'] = 'Unknown';
     }
-    
+
     return $headers;
 }
 
+function get_vulns( $extension_slug, $extension_type ) {
+    $api_type_path = $extension_type === 'plugin' ? 'plugins' : 'themes';
+    $api_url = "https://wpscan.com/api/v3/$api_type_path/{$extension_slug}";
+    return make_wpscan_api_request( $api_url );
+}
 
-$wpscan_token = $_ENV['WPSCAN_TOKEN'];
+function make_wpscan_api_request( $api_url ) {
+    $token = $_ENV['WPSCAN_TOKEN'];
+
+    $options = array(
+        'http'=>array(
+          'method'=>"GET",
+          'header'=>"Authorization: Token token=$token\r\n"
+        )
+      );
+
+    $context = stream_context_create( $options );
+
+  	$result = file_get_contents( $api_url, false, $context );
+
+  	$parsed = json_decode( $result, true );
+
+  	$response = [];
+  	$vulnerabilities = array_values( $parsed )[0]['vulnerabilities'];
+  	foreach ( $vulnerabilities as $vulnerability ) {
+ 		array_push( $response, [ 'id' => $vulnerability['id'], 'title' => $vulnerability['title'], 'fixed_in' => $vulnerability['fixed_in'], 'description' => $vulnerability['description'] ] );
+ 	}
+  	return $response;
+ }
+
 $wp_content_path = $_ENV['WP_CONTENT_PATH'];
 
-echo $wpscan_token;
+$extensions = get_plugins_and_themes( $wp_content_path );
 
-$result = getPluginsAndThemes($wp_content_path);
-var_dump($result);
+$found_vulns = false;
 
-exit(1);
+foreach ( $extensions as $extension ) {
+    $extension['vulnerabilities'] = array_filter(
+        get_vulns( $extension['slug'], $extension['type'] ),
+        function( $vuln ) use ( $extension ) {
+            return version_compare( $extension['version'], $vuln['fixed_in'], '<' );
+        }
+    );
+
+    if( ! empty( $extension['vulnerabilities'] ) ) {
+        $found_vulns = true;
+        echo( "Found vulnerability in the following extension: " );
+        var_dump( $extension );
+    }
+}
+
+if( $found_vulns ){
+    exit(1);
+}
+
+exit(0);
